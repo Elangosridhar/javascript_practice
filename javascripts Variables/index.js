4b43ba8d602fff1ea50c1981a20cdaa4d131bdb8
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-03-27 03:51:04 +0530

    Add files via upload

---
+++ javascripts Variables/index.js
@@ -0,0 +1,54 @@
+// variable = A container that stores a value. Behave as if it were the value it contains.
+
+// 1. declaration let x;
+//2. assignment x = 100;
+
+// a variable need to unique
+
+let fullName = "Elango Sridhar";
+let age = 20;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`
+document.getElementById("p2").textContent = `you are ${age} years old`
+document.getElementById("p3").textContent = `Enrolled: ${student}`
+
+
+
+
+
+
+
+
+// let x;
+
+// x =100
+
+// console.log(x);
+// numbers
+
+// let age = 25;
+// let price = 10.99;
+// let gpa =2.1;
+
+
+// strings
+// let firstName = "Elango";
+// let favouriteFood = "pizza";
+
+// Booleans
+// let online =true;
+// let forSale = false;
+
+
+
+// console.log(`bro is online: ${online}`)
+// console.log(`is this car for sale: ${forSale}`)
+
+// console.log(typeof firstName);
+// console.log(`your name is ${firstName}`);
+// console.log(`you like ${favouriteFood}`)
+// console.log(typeof gpa)
+// console.log(`you are ${age} years old`);
+// console.log(`The price is  ${price} `);
+// console.log(`your gpa is: ${gpa}`);
