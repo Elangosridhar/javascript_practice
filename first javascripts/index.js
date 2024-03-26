bad52417f6176a2f17a9cb05c60403afe069caef
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-03-27 03:08:52 +0530

    Add files via upload

---
+++ first javascripts/index.js
@@ -0,0 +1,10 @@
+// console.log('Hello');
+// console.log('I  like pizza!')
+
+// window.alert('This is an alert')
+// window.alert('I like pizza')
+
+// This is  a comment
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myp").textContent = 'I like Pizza';
